abff2553657b20f16fc2281c2905d2a962e42ec5
saurabh <devs.saurabh@gmail.com>
2018-11-27 10:15:07 +0530

    initial commit to setup express server

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://devsaurabh:saurabh123@ds019796.mlab.com:19796/devconnector"
+};
---
+++ routes/api/profile.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+//@route   api/profile/test
+//@desc    Test profile route
+//@access  public
+router.get("/test", (req, res) => res.json({ msg: "Profile Works!" }));
+
+module.exports = router;
