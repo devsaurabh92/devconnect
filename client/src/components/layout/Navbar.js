8acc28a29465783de77a38532e38a0ca02cdd78e
saurabh <devs.saurabh@gmail.com>
2018-12-21 12:56:05 +0530

    edit profile

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -18,7 +18,7 @@ class Navbar extends Component {
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
           <Link className="nav-link" to="/dashboard">
-            Sign Up
+            Dashboard
           </Link>
         </li>
         <li className="nav-item">
